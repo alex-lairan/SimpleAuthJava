f2893d5ac15fc59a5691f2a93ac7b2f81db21ccb
Tristan <TristanEduProjet@users.noreply.github.com>
2017-06-04 17:18:54 +0200

    Simplified hash to string method Add javadoc

--- src/main/java/fr/esgi/simple_auth_java/password_encrypt/PasswordEncrypt.java
+++ src/main/java/fr/esgi/simple_auth_java/password_encrypt/PasswordEncrypt.java
@@ -1,36 +1,34 @@
 package fr.esgi.simple_auth_java.password_encrypt;
 
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Password Encrypt.
  *
- * @author Alexandre
+ * @author Alexandre, Tristan
  */
+@RequiredArgsConstructor
 public class PasswordEncrypt {
     @NonNull private final String password;
 
-    public PasswordEncrypt(@NonNull String password) {
-        this.password = password;
-    }
-
+    /**
+     * Calculate the password's SHA-256 hash
+     * @return SHA-256 of string password
+     * @throws RuntimeException if error
+     */
     public String encrypt() {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(password.getBytes("UTF-8"));
-            StringBuilder hexString = new StringBuilder();
-
-            for (byte aHash : hash) {
-                String hex = Integer.toHexString(0xff & aHash);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
+            return new BigInteger(1, hash).toString(16);
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error while hashed password", e);
         }
     }
 }
