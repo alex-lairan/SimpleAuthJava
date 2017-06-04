c58d1b74137ced72216b388a76a4b7cec693fbe2
alex-lairan <lairana@free.fr>
2017-06-04 13:18:30 +0200

    Add class User implementation

--- src/main/java/fr/esgi/simple_auth_java/Main.java
+++ src/main/java/fr/esgi/simple_auth_java/Main.java
@@ -1,8 +1,5 @@
 package fr.esgi.simple_auth_java;
 
-/**
- * Created by lairan on 04/06/2017.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
--- src/main/java/fr/esgi/simple_auth_java/User.java
+++ src/main/java/fr/esgi/simple_auth_java/User.java
@@ -1,15 +1,40 @@
 package fr.esgi.simple_auth_java;
 
+import fr.esgi.simple_auth_java.password_encrypt.PasswordEncrypt;
 import lombok.Data;
 import lombok.NonNull;
+
+import java.util.HashMap;
 
 /**
  * User bean.
  *
- * @author Tristan
+ * @author Tristan, Alexandre
  */
 @Data
 public class User {
-    @NonNull String email, first_name, last_name;
-    @NonNull String password;
+    @NonNull private String email, first_name, last_name;
+    @NonNull private String password;
+    @NonNull private Boolean is_connected = false;
+
+    public User(HashMap<String, String> args) {
+        email      = args.get("email");
+        first_name = args.get("first_name");
+        last_name  = args.get("last_name");
+
+        PasswordEncrypt encryptor = new PasswordEncrypt(args.get("password"));
+        password = encryptor.encrypt();
+    }
+
+    public Boolean isAuthenticated() {
+        return is_connected;
+    }
+
+    public void connect() {
+        is_connected = true;
+    }
+
+    public void disconnect() {
+        is_connected = false;
+    }
 }
