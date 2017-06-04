6cc80295cbf159f7baef9d9046e9ac3c2851c980
Tristan <TristanEduProjet@users.noreply.github.com>
2017-06-04 18:54:08 +0200

    Add constraints on User bean with tests

--- src/main/java/fr/esgi/simple_auth_java/User.java
+++ src/main/java/fr/esgi/simple_auth_java/User.java
@@ -2,7 +2,10 @@ package fr.esgi.simple_auth_java;
 
 import fr.esgi.simple_auth_java.password_encrypt.PasswordEncrypt;
 import lombok.*;
+import org.hibernate.validator.constraints.Email;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.HashMap;
 
 /**
@@ -13,24 +16,25 @@ import java.util.HashMap;
 @Data
 @RequiredArgsConstructor
 public class User {
-    @NonNull private String email, first_name, last_name;
-    @NonNull private String password;
+    @NotNull @NonNull @Email private String email;
+    @NotNull @NonNull private String first_name, last_name;
+    @NotNull @NonNull @Size(min=4) private String password;
     @Setter(AccessLevel.PROTECTED) @Getter(AccessLevel.PROTECTED)
-    @NonNull private Boolean isConnected = false;
+    private boolean isConnected = false;
 
     public User(HashMap<String, String> args) {
         this(args.get("email"), args.get("first_name"), args.get("last_name"), new PasswordEncrypt(args.get("password")).encrypt());
     }
 
-    public Boolean isAuthenticated() {
-        return this.getIsConnected();
+    public boolean isAuthenticated() {
+        return this.isConnected();
     }
 
     public void connect() {
-        this.setIsConnected(true);
+        this.setConnected(true);
     }
 
     public void disconnect() {
-        this.setIsConnected(false);
+        this.setConnected(false);
     }
 }
