eb163d1666238ade4efcfbc13cb9f61bcb50d8fd
alex-lairan <lairana@free.fr>
2017-06-10 17:35:40 +0200

    Create Auth method for user

--- src/main/java/fr/esgi/simple_auth_java/Manager.java
+++ src/main/java/fr/esgi/simple_auth_java/Manager.java
@@ -1,22 +1,25 @@
 package fr.esgi.simple_auth_java;
 
-import fr.esgi.simple_auth_java.auth.Authentificator;
+import fr.esgi.simple_auth_java.auth.AuthenticationException;
+import fr.esgi.simple_auth_java.auth.Authenticator;
 import fr.esgi.simple_auth_java.register.Registor;
 import fr.esgi.simple_auth_java.register.SignUpException;
-import fr.esgi.simple_auth_java.reset.IllegalResetException;
-import fr.esgi.simple_auth_java.reset.ResetException;
 import fr.esgi.simple_auth_java.reset.Resetor;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Manage actions on a user.
  *
  * @author Tristan
  * @see Registor
- * @see Authentificator
+ * @see Authenticator
  * @see Resetor
  * @see User
  */
@@ -38,12 +41,17 @@ public final class Manager {
 
     /**
      * Sign In a user with a system
-     * @param authentificator the system / implementation
+     * @param authenticator the system / implementation
      * @return th user identified
      */
-    public User signIn(@NonNull final Authentificator authentificator) {
-        log.trace("signIn with {}", authentificator);
-        final User result = authentificator.signIn();
+    public User signIn(@NonNull final Authenticator authenticator) {
+        log.trace("signIn with {}", authenticator);
+        User result = null;
+        try {
+            result = authenticator.signIn(new HashMap<>(), new ArrayList<User>());
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
         log.trace("signUp user : {}", result);
         return result;
     }
