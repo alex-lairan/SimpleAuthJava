e984e3aec826e667f7254275b90a5e3d99625c7e
Nicolas Cornu <nicolas.demoncourt@gmail.com>
2017-06-11 19:10:21 +0200

    Integ. Test + Exception tests on each.

--- src/test/java/fr/esgi/simple_auth_java/register/RegisterDBTest.java
+++ src/test/java/fr/esgi/simple_auth_java/register/RegisterDBTest.java
@@ -2,13 +2,16 @@ package fr.esgi.simple_auth_java.register;
 
 import fr.esgi.simple_auth_java.User;
 import fr.esgi.simple_auth_java.password_encrypt.PasswordEncryptSHA256;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.Test;
 import org.mockito.Mock;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -48,35 +51,74 @@ public class RegisterDBTest {
         assertEquals(email, testUser.getEmail());
         assertEquals(firstName, testUser.getFirst_name());
         assertEquals(lastName, testUser.getLast_name());
-        assertEquals(new PasswordEncryptSHA256().encrypt(password), testUser.getPassword());
+        assertEquals(testUser.getEncryptor().encrypt(password), testUser.getPassword());
     }
 
-    // Maybe finish this test if we find a solution to Mock Scanner/System.In jobs
-    /*@Mock Scanner mockedScanner;
-    @Test
-    public void mockSignUp() throws Exception {
+    @Test(expected = SignUpException.class)
+    public void signUpException() throws Exception {
+        //Init variables.
         String email = "testsignup@esgitu.com";
         String firstName = "Sign";
         String lastName = "Up";
         String password = "mysuperpasswd";
 
-        when(mockedScanner.next()).thenReturn(email).thenReturn(firstName).thenReturn(lastName).thenReturn(password);
+        //Change them into 1 line configuration.
+        String simulatedUserInput = email + System.getProperty("line.separator") +
+                firstName + System.getProperty("line.separator") +
+                lastName + System.getProperty("line.separator");
 
-        User testUser = new RegisterDB().signUp();
+        //Convert it to a ByteArray of the System.In
+        ByteArrayInputStream setIn = new ByteArrayInputStream(simulatedUserInput.getBytes());
 
-        //Assertation
-        //  - Testing if same class
-        assertEquals(User.class, testUser.getClass());
-        //  - Testing his vars
-        assertEquals(email, testUser.getEmail());
-        assertEquals(firstName, testUser.getFirst_name());
-        assertEquals(lastName, testUser.getLast_name());
-        assertEquals(new PasswordEncryptSHA256().encrypt(password), testUser.getPassword());
-    }*/
+        //Set it into System.In and called when System.In.next() fire.
+        System.setIn(setIn);
+
+        //Call function
+        new RegisterDB().signUp();
+    }
 
     @Test
     public void insertDb() throws Exception {
-        //This will be the Integ. Test
+        String email = "testsignup@esgitu.com";
+        String firstName = "Sign";
+        String lastName = "Up";
+        String password = "mysuperpasswd";
+
+        //Change them into 1 line configuration.
+        String simulatedUserInput = email + System.getProperty("line.separator") +
+                firstName + System.getProperty("line.separator") +
+                lastName + System.getProperty("line.separator") +
+                password + System.getProperty("line.separator");
+
+        //Convert it to a ByteArray of the System.In
+        ByteArrayInputStream setIn = new ByteArrayInputStream(simulatedUserInput.getBytes());
+
+        //Set it into System.In and called when System.In.next() fire.
+        System.setIn(setIn);
+
+        new RegisterDB().insertDb();
+    }
+
+    @Test(expected = SignUpException.class)
+    public void insertDbException() throws Exception {
+        String email = "testsignup@esgitu.com";
+        String firstName = "\'Sign\'";
+        String lastName = "Up";
+        String password = "mysuperpasswd";
+
+        //Change them into 1 line configuration.
+        String simulatedUserInput = email + System.getProperty("line.separator") +
+                firstName + System.getProperty("line.separator") +
+                lastName + System.getProperty("line.separator") +
+                password + System.getProperty("line.separator");
+
+        //Convert it to a ByteArray of the System.In
+        ByteArrayInputStream setIn = new ByteArrayInputStream(simulatedUserInput.getBytes());
+
+        //Set it into System.In and called when System.In.next() fire.
+        System.setIn(setIn);
+
+        new RegisterDB().insertDb();
     }
 
 }
